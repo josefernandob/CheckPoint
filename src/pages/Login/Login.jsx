55048386fb44031aac73ed6cbe584ff9581cc174
José Fernando <145086836+josefernandob@users.noreply.github.com>
2025-04-23 19:27:58 +0000

    deploy1.1

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -14,16 +14,25 @@ export function Login() {
 
             <div className={styles.Inputs}>
 
-            <img src="/src/assets/User.svg" />
+                <div className={styles.User}> 
+            <img src="/src/assets/User.svg" /> 
+                <p> | </p>
                 <input type="text" placeholder="Usuário" />
 
+                </div>
 
-
+                <div className={styles.Senha}> 
             <img src="/src/assets/Lock.svg" />
+              <p> | </p>
                 <input type="text" placeholder="Senha" />
+                </div>
 
             </div>
             <a href="#">Esqueceu sua senha?</a>
+
+            
+
+
             <button type="submit">Entrar</button>
 
         </form>
